e5479310f66f34a19b2ad61998b7f94b38678361
unknown <michaelblob99@gmail.com>
2015-11-23 19:21:38 -0500

    Started Servo Class

---
+++ RMRoboticsLibrary/src/main/java/com/rmrobotics/library/Servo.java
@@ -0,0 +1,13 @@
+package com.rmrobotics.library.control;
+
+/**
+ * Created by RM Robotics on 11/23/2015.
+ */
+abstract public class Servo {
+
+    private boolean isContinuous;
+    private int minValue;
+    private int maxValue;
+
+
+}
