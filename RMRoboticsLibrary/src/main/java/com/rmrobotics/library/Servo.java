ec6f62fbab8e4b327bf751c5c5ea9fdc9e9a0673
Simon Liu <michaelblob99@gmail.com>
2015-11-25 19:00:25 -0500

    Added servo constructor and set method

--- RMRoboticsLibrary/src/main/java/com/rmrobotics/library/Servo.java
+++ RMRoboticsLibrary/src/main/java/com/rmrobotics/library/Servo.java
@@ -6,8 +6,35 @@ package com.rmrobotics.library;
 abstract public class Servo {
 
     private boolean isContinuous;
-    private int minValue;
-    private int maxValue;
+    private double minValue;
+    private double maxValue;
+    private double power;
 
+    public Servo(boolean x){
+        isContinuous = x;
+        if(!isContinuous){
+            maxValue = 0.9;
+            minValue = 0.1;
+        } else {
+            maxValue = 1;
+            minValue = 0;
+        }
+    }
+
+    public void set(double x) {
+        power = x;
+        speedCheck();
+        //TODO: create statement to feed power value to servo
+    }
+
+    private int speedCheck(){
+        if(power > maxValue){
+            power = maxValue;
+        } else if(power < minValue){
+            power = minValue;
+        }
+    }
+
+//TODO: create method to scale percentage or RobotC values to current Java values
 
 }
