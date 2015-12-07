4dddaa46842dcf18507ace79b401d041e7693d6b
Joshua Chen <chen.joshua.2017@gmail.com>
2015-12-07 16:55:07 -0500

    Create joyDrive class

---
+++ RMRoboticsLibrary/src/main/java/com/rmrobotics/library/joyDrive.java
@@ -0,0 +1,21 @@
+package com.rmrobotics.library;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+import com.rmrobotics.library.Motor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.rmrobotics.library.control.Axis;
+import com.rmrobotics.library.control.Control;
+import com.rmrobotics.library.control.Controller;
+import com.rmrobotics.library.control.Joystick;
+
+
+/**
+ * Created by JaJaJaJaJa on 12/7/15.
+ */
+public class joyDrive {
+    public joyDrive(){
+
+
+    }
+}
