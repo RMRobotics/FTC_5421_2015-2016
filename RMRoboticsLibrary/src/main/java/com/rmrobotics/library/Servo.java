1f39f60d017564a73b0b5f1508454d3a43213eb7
Simon Liu <michaelblob99@gmail.com>
2015-12-06 20:06:08 -0500

    Revert back to custom class servo

--- RMRoboticsLibrary/src/main/java/com/rmrobotics/library/Servo.java
+++ RMRoboticsLibrary/src/main/java/com/rmrobotics/library/Servo.java
@@ -7,28 +7,39 @@ import com.qualcomm.robotcore.util.Range;
  * Created by RM Robotics on 11/23/2015.
  */
 
-public class rServo extends Servo {
+public class rServo{
 
+    private static final double MAX_POWER = 1.0;
+    private static final double MIN_POWER = 0.1;
+
+    private Servo parent;
+    private Servo.Direction defaultDirection;
     private double minPower;
     private double maxPower;
     private double desiredPower;
     private double currentPower;
     private boolean isContinuous;
 
-    public rServo(boolean x) {
+    public rServo(Servo s, boolean x){
+        parent = s;
         isContinuous = x;
-        if (!isContinuous) {
+        if(!isContinuous){
             maxPower = 0.9;
             minPower = 0.1;
         } else {
             maxPower = 1;
-            minPower = 0.1;
+            minPower = 0;
         }
     }
 
     public void set(double x) {
         desiredPower = x;
-        currentPower = Range.clip(desiredPower, minPower, maxPower);
-        setPosition(currentPower);
+        currentPower = speedCheck(desiredPower);
+        //TODO feed power to servo
     }
+
+    private double speedCheck(double p){
+        p = Range.clip(minPower, maxPower);
+    }
+
 }
