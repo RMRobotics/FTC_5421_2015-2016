47c2e2634c2e165034cb24a9067cc76ccd0bf30d
Simon Liu <michaelblob99@gmail.com>
2016-01-02 18:28:56 -0500

    Change state from String to int

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/AutoState.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/AutoState.java
@@ -9,46 +9,108 @@ import java.util.Calendar;
  */
 
 public class AutoState extends RMOpMode {
-    private String state = "begin";
+    private int state = 1;
     Calendar cal;
     protected long curTime;
     protected long startTime;
 
+    private final String CONFIGURATION_PATH = "{\n" +
+            "  \"motors\":[\n" +
+            "    {\n" +
+            "      \"name\":\"DriveLeftOne\",\n" +
+            "      \"minPower\":0.1,\n" +
+            "      \"maxPower\":1.0,\n" +
+            "      \"direction\":\"REVERSE\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"name\":\"DriveLeftTwo\",\n" +
+            "      \"minPower\":0.1,\n" +
+            "      \"maxPower\":1.0,\n" +
+            "      \"direction\":\"REVERSE\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"name\":\"DriveRightOne\",\n" +
+            "      \"minPower\":0.1,\n" +
+            "      \"maxPower\":1.0,\n" +
+            "      \"direction\":\"FORWARD\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"name\":\"DriveRightTwo\",\n" +
+            "      \"minPower\":0.1,\n" +
+            "      \"maxPower\":1.0,\n" +
+            "      \"direction\":\"FORWARD\"\n" +
+            "    },\n" +
+            "     {\\n\" +\n" +
+            "            \"     \"name\":\"Harvester\",\n" +
+            "      \"minPower\":0.1,\n" +
+            "      \"maxPower\":1.0,\n" +
+            "      \"direction\":\"REVERSE\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"name\":\"Bucket\",\n" +
+            "      \"minPower\":0.1,\n" +
+            "      \"maxPower\":1.0,\n" +
+            "      \"direction\":\"FORWARD\"\n" +
+            "    }\n" +
+            "  ],\n" +
+            "  \"servos\":[\n" +
+            "    {\n" +
+            "      \"name\":\"BucketRight\",\n" +
+            "      \"minPosition\":0.01,\n" +
+            "      \"maxPosition\":1.0,\n" +
+            "      \"direction\":\"FORWARD\",\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"name\":\"BucketLeft\",\n" +
+            "      \"minPosition\":0.01,\n" +
+            "      \"maxPosition\":1.0,\n" +
+            "      \"direction\":\"FORWARD\",\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"name\":\"Climbers\",\n" +
+            "      \"minPosition\":0.01,\n" +
+            "      \"maxPosition\":1.0,\n" +
+            "      \"direction\":\"FORWARD\",\n" +
+            "    }\n" +
+            "  ]\n" +
+            "}";
+
     @Override
     public void init() {
         super.init();
+
         cal = Calendar.getInstance();
     }
 
+    @Override
+    protected String setConfigurationPath() {
+        return CONFIGURATION_PATH;
+    }
+
     public void calculate() {
-        while (state != "end") {
+        while (state != 0) {
             switch (state) {
-                case "begin":
-                    state = "center";
-                case "center":
+                case 1: //begin
+                    state = 2;
+                case 2: //drive to center
                     motorMap.get("DriveLeftOne").setDesiredPower(1.0);
                     motorMap.get("DriveLeftTwo").setDesiredPower(1.0);
                     motorMap.get("DriveRightOne").setDesiredPower(1.0);
                     motorMap.get("DriveRightTwo").setDesiredPower(1.0);
                     waitTime(1000);
-                    state = "turnLeft45";
-                case "turnLeft45":
+                    state = 3;
+                case 3: //turn left 45 degree
                     motorMap.get("DriveLeftOne").setDesiredPower(0.0);
                     motorMap.get("DriveLeftTwo").setDesiredPower(0.0);
                     motorMap.get("DriveRightOne").setDesiredPower(1.0);
                     motorMap.get("DriveRightTwo").setDesiredPower(1.0);
                     waitTime(1000);
-                    state = "beaconZone";
-                case "beaconZone":
+                    state = 4;
+                case 4: //drive to beacon zone
                     
 
 
    /* Check conditions and possibly modify state */
-
-                    state = STATE1;
-
-                    if (someQuitCheck == true) state = QUIT;
-                    break;
                 default:
                     break;
             } // end switch
