1302a5839a8732d5bd9fbebac3fbcff3e69518e0
Simon Liu <michaelblob99@gmail.com>
2016-01-13 22:37:37 -0500

    Merging

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -1,22 +1,16 @@
 /* Copyright (c) 2014, 2015 Qualcomm Technologies Inc
-
 All rights reserved.
-
 Redistribution and use in source and binary forms, with or without modification,
 are permitted (subject to the limitations in the disclaimer below) provided that
 the following conditions are met:
-
 Redistributions of source code must retain the above copyright notice, this list
 of conditions and the following disclaimer.
-
 Redistributions in binary form must reproduce the above copyright notice, this
 list of conditions and the following disclaimer in the documentation and/or
 other materials provided with the distribution.
-
 Neither the name of Qualcomm Technologies Inc nor the names of its contributors
 may be used to endorse or promote products derived from this software without
 specific prior written permission.
-
 NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
 LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -31,10 +25,11 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
-import com.qualcomm.ftcrobotcontroller.Auto8121;
 import com.qualcomm.ftcrobotcontroller.AutoOp;
+import com.qualcomm.ftcrobotcontroller.AutoState;
 import com.qualcomm.ftcrobotcontroller.EncoderTest;
 import com.qualcomm.ftcrobotcontroller.R2D2;
+import com.qualcomm.ftcrobotcontroller.ServoCalibration;
 import com.qualcomm.ftcrobotcontroller.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeRegister;
@@ -63,10 +58,10 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("NullOp", NullOp.class);
 
     //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
-   // manager.register("K9TeleOp", K9TeleOp.class);
-   // manager.register("K9Line", K9Line.class);
-   // manager.register ("PushBotAuto", PushBotAuto.class);
-    //manager.register ("PushBotManual", PushBotManual.class);
+    manager.register("K9TeleOp", K9TeleOp.class);
+    manager.register("K9Line", K9Line.class);
+    manager.register ("PushBotAuto", PushBotAuto.class);
+    manager.register ("PushBotManual", PushBotManual.class);
 
 
 
@@ -77,21 +72,23 @@ public class FtcOpModeRegister implements OpModeRegister {
 
     manager.register("TeleOp", TeleOp.class);
     manager.register("AutoOp", AutoOp.class);
+    manager.register("AutoState", AutoState.class);
     manager.register("R2D2", R2D2.class);
-    manager.register("Auto8121", Auto8121.class);
 
-   // manager.register("navXCollisionDetectionOp", navXCollisionDetectionOp.class);
-   // manager.register("navXDriveStraightPIDLinearOp", navXDriveStraightPIDLinearOp.class);
-   // manager.register("navXDriveStraightPIDLoopOp", navXDriveStraightPIDLoopOp.class);
-   // manager.register("navXMotionDetectionOp", navXMotionDetectionOp.class);
-   // manager.register("navXPerformanceTuningOp", navXPerformanceTuningOp.class);
-   // manager.register("navXProcessedOp", navXProcessedOp.class);
-   // manager.register("navXRawOp", navXRawOp.class);
-   // manager.register("navXRotateToAnglePIDLinearOp", navXRotateToAnglePIDLinearOp.class);
-   // manager.register("navXRotateToAnglePIDLoopOp", navXRotateToAnglePIDLoopOp.class);
-   // manager.register("navXZeroYawOp", navXZeroYawOp.class);
+    manager.register("ServoCalibration", ServoCalibration.class);
 
-    manager.register("EncoderTest", EncoderTest.class);
+    //manager.register("navXCollisionDetectionOp", navXCollisionDetectionOp.class);
+    //manager.register("navXDriveStraightPIDLinearOp", navXDriveStraightPIDLinearOp.class);
+    //manager.register("navXDriveStraightPIDLoopOp", navXDriveStraightPIDLoopOp.class);
+    //manager.register("navXMotionDetectionOp", navXMotionDetectionOp.class);
+    //manager.register("navXPerformanceTuningOp", navXPerformanceTuningOp.class);
+    //manager.register("navXProcessedOp", navXProcessedOp.class);
+    //manager.register("navXRawOp", navXRawOp.class);
+    //manager.register("navXRotateToAnglePIDLinearOp", navXRotateToAnglePIDLinearOp.class);
+    //manager.register("navXRotateToAnglePIDLoopOp", navXRotateToAnglePIDLoopOp.class);
+    //manager.register("navXZeroYawOp", navXZeroYawOp.class);
+
+    //manager.register("EncoderTest", EncoderTest.class);
 
     //manager.register("AdafruitRGBExample", AdafruitRGBExample.class);
     //manager.register("ColorSensorDriver", ColorSensorDriver.class);
