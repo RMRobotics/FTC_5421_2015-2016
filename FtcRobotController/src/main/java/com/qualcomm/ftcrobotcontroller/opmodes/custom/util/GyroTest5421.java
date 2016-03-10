7351f9f517f5e8a3e5c1da8f3a51ac79fb63672e
Simon Liu <michaelblob99@gmail.com>
2016-03-09 22:59:57 -0500

    Add more functionality to gyro test

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/util/GyroTest5421.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/util/GyroTest5421.java
@@ -18,18 +18,28 @@ public class GyroTest5421 extends OpMode {
         gyro = hardwareMap.gyroSensor.get("gyro");
         gyro.calibrate();
         //mL = hardwareMap.dcMotor.get("mL");
+        //mL.setDirection(DcMotor.Direction.REVERSE);
         //mR = hardwareMap.dcMotor.get("mR");
-        //mR.setDirection(DcMotor.Direction.REVERSE);
         //g1 = gamepad1;
+    }
+
+    public void init_loop() {
+        telemetry.addData("GYRO CALIBRATING:", gyro.isCalibrating());
     }
 
     @Override
     public void loop() {
         //double leftPower = g1.left_stick_y;
         //double rightPower = g1.right_stick_y;
-        //mL.setPower(leftPower);
-        //mR.setPower(rightPower);
+        //mL.setPower(leftPower*0.5);
+        //mR.setPower(rightPower*0.5);
 
-        telemetry.addData("X-Y-Z-H", gyro.rawX() + "-" + gyro.rawY() + "-" + gyro.rawZ() + "-" + gyro.getHeading());
+        //telemetry.addData("X-Y-Z-H", gyro.rawX() + "-" + gyro.rawY() + "-" + gyro.rawZ() + "-" + gyro.getHeading());
+
+        telemetry.addData("1. x", String.format("%03d", gyro.rawX()));
+        telemetry.addData("2. y", String.format("%03d", gyro.rawY()));
+        telemetry.addData("3. z", String.format("%03d", gyro.rawZ()));
+        telemetry.addData("4. h", String.format("%03d", gyro.getHeading()));
+        telemetry.addData("5. t", String.format("%05d", getRuntime()));
     }
 }
